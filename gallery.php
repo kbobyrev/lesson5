1740a7bfe94defab18442681562843ea19138e97
kbobyrev <61451444+kbobyrev@users.noreply.github.com>
2020-09-07 02:58:42 +0300

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <title>Галерея</title>
+    <link rel="stylesheet" href="css/styles.css"> 
+</head>
+	<body>
+		<div class="main">
+			<?
+			include "showAllimg.php";
+			echo $_PATH;
+			?>
+		</div>
+	</body>
+</html>
